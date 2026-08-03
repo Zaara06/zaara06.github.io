7de4b9cab93ba3862eb7a81329ae7eeba32d2ad6
Zaara06 <zaara.murtuza@ontariotechu.net>
2026-08-03 19:44:41 -0400

    modified the starter files js

--- assignment4/part3/main.js
+++ assignment4/part3/main.js
@@ -21,16 +21,57 @@ class Ball {
   }
 }
 class Ball {
-  // …
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
   draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
   }
-}
-const testBall = new Ball(50, 100, 4, 4, "blue", 10);
-testBall.x;
+
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}testBall.x;
 testBall.size;
 testBall.color;
 testBall.draw();
@@ -74,18 +115,6 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
-function loop() {
-  ctx.fillStyle = "rgb(0 0 0 / 25%)";
-  ctx.fillRect(0, 0, width, height);
-
-  for (const ball of balls) {
-    ball.draw();
-    ball.update();
-  }
-
-  requestAnimationFrame(loop);
-}
-loop();
 class Ball {
   // …
   collisionDetect() {
@@ -114,3 +143,5 @@ function loop() {
 
   requestAnimationFrame(loop);
 }
+
+loop();
