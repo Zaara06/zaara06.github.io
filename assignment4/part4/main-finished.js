95741091b9385474960f59b930e6607cdcaa47e8
Zaara06 <zaara.murtuza@ontariotechu.net>
2026-08-03 22:59:54 -0400

    replaced collision detect

--- assignment4/part4/main-finished.js
+++ assignment4/part4/main-finished.js
@@ -17,22 +17,16 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-class Shape {
-  constructor(x, y, velX, velY) {
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
-  }
-}
-class Ball extends Shape {
- constructor(x, y, velX, velY, color, size) {
-    super(x, y, velX, velY);
-
     this.color = color;
     this.size = size;
-    this.exists = true;
-}
+  }
 
   draw() {
     ctx.beginPath();
@@ -63,14 +57,15 @@ class Ball extends Shape {
   }
 
  collisionDetect() {
-  for (const ball of balls) {
-    if (!(this === ball) && ball.exists) {
-      const dx = this.x - ball.x;
-      const dy = this.y - ball.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
+    for (const ball of balls) {
+      if (!(this === ball) && ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
 
-      if (distance < this.size + ball.size) {
-        ball.color = this.color = randomRGB();
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
       }
     }
   }
